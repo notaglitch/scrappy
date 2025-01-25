00d24b957af38d0419527b76f6dbd2b4e34d06c6
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-01-25 21:20:14 +0100

    update

--- scrappy.py
+++ scrappy.py
@@ -16,35 +16,38 @@ locations = []
 languages = []
 
 for job in job_listings:
-    company = job.find('div', class_='table-col-1')
-    if company:
-        companies.append(company.text.strip())
-    else:
-        companies.append("N/A")
+    if 'head-part' in job.get('class', []):
+        continue
+    if 'result-info' not in job.get('class', []):
+        company = job.find('div', class_='table-col-1')
+        if company:
+            companies.append(company.text.strip())
+        else:
+            companies.append("N/A")
 
-    job_title = job.find('div', class_='table-col-2')
-    if job_title:
-        job_titles.append(job_title.text.strip())
-    else:
-        job_titles.append("N/A")
+        job_title = job.find('div', class_='table-col-2')
+        if job_title:
+            job_titles.append(job_title.text.strip())
+        else:
+            job_titles.append("N/A")
 
-    location = job.find('div', class_='table-col-3')
-    if location:
-        locations.append(location.text.strip())
-    else:
-        locations.append("N/A")
+        location = job.find('div', class_='table-col-3')
+        if location:
+            locations.append(location.text.strip())
+        else:
+            locations.append("N/A")
 
-    language = job.find('div', class_='table-col-4')
-    if language:
-        languages.append(language.text.strip())
-    else:
-        languages.append("N/A")
+        language = job.find('div', class_='table-col-4')
+        if language:
+            languages.append(language.text.strip())
+        else:
+            languages.append("N/A")
 
 data = {
-    'Company': companies,
-    'Job Title': job_titles,
-    'Location': locations,
-    'Language': languages
+    companies[0]: companies,
+    job_titles[0]: job_titles,
+    locations[0]: locations,
+    languages[0]: languages
 }
 
 df = pd.DataFrame(data)
@@ -52,4 +55,3 @@ df = pd.DataFrame(data)
 df.to_csv('job_listing.csv', index=False)
 
 print("Data has been saved successfully")
-print(job_listings)
