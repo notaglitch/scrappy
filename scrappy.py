092686cf55f9036a363b2727abaf0f2f32c7a494
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-01-26 20:29:23 +0100

    updated

--- scrappy.py
+++ scrappy.py
@@ -1,7 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from openpyxl import load_workbook
+
+# Set the path to the chromedriver
+chromedriver_path = r"C:\Program Files\Google\Chrome\Driver\chromedriver-win64\chromedriver.exe"
+service = Service(chromedriver_path)
+driver = webdriver.Chrome(service=service)
 
 
 def decrypt_email(encrypted_str, encryption_key="EncryptionKey"):
@@ -22,11 +33,44 @@ def decrypt_email(encrypted_str, encryption_key="EncryptionKey"):
     return decrypted_str
 
 
-url = 'https://www.orientation.ch/dyn/show/170785?lang=fr&Idx=20&OrderBy=1&Order=0&PostBackOrder=0&postBack=true&CountResult=446&Total_Idx=446&CounterSearch=8&UrlAjaxWebSearch=%2FLeFiWeb%2FAjaxWebSearch&getTotal=True&isBlankState=False&prof_=68800.1&fakelocalityremember=&LocName=Neuch%C3%A2tel&LocId=neuchatel-ne-ch&Area=10&LocatorVM.Area=10&langcode_=de&langcode_=fr&langcode_=it&langcode_=rm&langcode_=en'
+# Open the website
+# url = 'https://www.orientation.ch/dyn/show/170785?lang=fr&Idx=20&OrderBy=1&Order=0&PostBackOrder=0&postBack=true&CountResult=27&Total_Idx=27&CounterSearch=10&UrlAjaxWebSearch=%2FLeFiWeb%2FAjaxWebSearch&getTotal=True&isBlankState=False&prof_=47419.1&fakelocalityremember=&LocName=Neuch%C3%A2tel&LocId=neuchatel-ne-ch&Area=10&LocatorVM.Area=10&langcode_=de&langcode_=fr&langcode_=it&langcode_=rm&langcode_=en'
+url = "https://www.orientation.ch/dyn/show/170785?lang=fr&Idx=20&OrderBy=1&Order=0&PostBackOrder=0&postBack=true&CountResult=446&Total_Idx=446&CounterSearch=26&UrlAjaxWebSearch=%2FLeFiWeb%2FAjaxWebSearch&getTotal=True&isBlankState=False&prof_=68800.1&fakelocalityremember=&LocName=Neuch%C3%A2tel&LocId=neuchatel-ne-ch&Area=10&AreaCriteria=10&langcode_=de&langcode_=fr&langcode_=it&langcode_=rm&langcode_=en"
+driver.get(url)
+# Wait for the page to load
+WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+    (By.XPATH, "//a[@id='aSearchPaging']")))
 
-response = requests.get(url)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+def click_more_results():
+    while True:
+        try:
+            # Wait for the "More results" button to be clickable
+            more_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//a[@id='aSearchPaging' and contains(text(), 'Afficher plus de résultats')]"))
+            )
+
+            # Click the "More" button
+            more_button.click()
+            print("Clicked 'More' button...")
+
+            # Wait for new results to load
+            time.sleep(3)
+        except Exception as e:
+            # Break the loop if the "More" button is not found (i.e., no more results to load)
+            print("No more 'More' buttons to click.")
+            break
+
+
+# Call the function to click the "More results" button
+click_more_results()
+
+# Once all content is loaded, get the page source
+page_source = driver.page_source
+
+# Parse the page with BeautifulSoup
+soup = BeautifulSoup(page_source, 'html.parser')
 
 job_listings = soup.find_all('div', class_='display-table-row')
 
@@ -35,26 +79,41 @@ job_titles = []
 locations = []
 languages = []
 addresses = []
+phone_numbers = []
+emails = []
 
-for job in job_listings[1:]:
+for job in job_listings:
     if 'result-info' not in job.get('class', []):
-        company = job.find(
-            'div', class_='table-col-1')
-        companies.append(company.text.strip() if company else "N/A")
+        company = job.find('div', class_='table-col-1')
+        if company:
+            if company.text != "Entreprise":
+                companies.append(company.text.strip())
+        else:
+            companies.append("N/A")
 
-        job_title = job.find(
-            'div', class_='table-col-2')
-        job_titles.append(job_title.text.strip() if job_title else "N/A")
+        job_title = job.find('div', class_='table-col-2')
+        if job_title:
+            if job_title.text != "Profession":
+                job_titles.append(job_title.text.strip())
+        else:
+            job_titles.append("N/A")
 
-        location = job.find(
-            'div', class_='table-col-3')
-        locations.append(location.text.strip() if location else "N/A")
+        location = job.find('div', class_='table-col-3')
+        if location:
+            if location.text != "Localité":
+                locations.append(location.text.strip())
+        else:
+            locations.append("N/A")
 
-        language = job.find(
-            'div', class_='table-col-4')
-        languages.append(language.text.strip() if language else "N/A")
+        language = job.find('div', class_='table-col-4')
+        if language:
+            if language.text != "Langue":
+                languages.append(language.text.strip())
+        else:
+            languages.append("N/A")
 
 job_contacts = soup.find_all('div', class_='result-elem-lower')
+
 for result in job_contacts:
     email_scripts = soup.find_all(
         'script', string=lambda s: s and 'sdbb.DecrypteEmail' in s)
@@ -62,6 +121,15 @@ for result in job_contacts:
         email_scripts = soup.find_all(
             'script', text=lambda t: t and 'sdbb.DecryptEmail' in t
         )
+    contact_section = result.find('div', class_='result-info-col columns w45')
+    phone_link = contact_section.find(
+        '', href=lambda href: href and href.startswith('tel:'))
+    if phone_link:
+        phone_number = phone_link.get('href').replace(
+            'tel:', '').strip()  # Get the number and clean it
+        phone_numbers.append(phone_number)
+    else:
+        phone_numbers.append("N/A")
 
     address_section = result.find('div', class_='w45')
     if address_section:
@@ -75,26 +143,24 @@ for result in job_contacts:
         addresses.append("N/A")
 
 
-keys = []
+emails = []
 
 for script in email_scripts:
     if script:
         script_text = script.string.strip()
         last_numbers = script_text.split("'")[5]
-        keys.append(last_numbers)
+        decrypted_email = decrypt_email(last_numbers)
+        emails.append(decrypted_email)
     else:
-        keys.append("NOT FOUND")
-
-for i, encrypted in enumerate(keys, 1):
-    decrypted_email = decrypt_email(encrypted)
-    print(f"Key {i}: {decrypted_email}")
+        emails.append("NOT FOUND")
 
 data = {
     "Company": companies,
     "Title": job_titles,
     "Location": locations,
     "Language": languages,
-    "Address": addresses
+    "Address": addresses,
+    "Email": emails
 }
 
 df = pd.DataFrame(data)
@@ -118,3 +184,5 @@ for col in ws.columns:
 wb.save('job_listing.xlsx')
 
 print("Data has been saved successfully")
+
+driver.quit()
